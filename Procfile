13a6f86839b18c7c7f092b0b932ac406b27cf581
ozrenv <ozren.vucicevic@gmail.com>
2023-10-20 12:57:20 +0200

    Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn djangowebpage.wsgi:application --log-file -
--- djangowebpage/settings.py
+++ djangowebpage/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+django_heroku.settings(locals())
